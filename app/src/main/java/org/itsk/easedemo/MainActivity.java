c1a7b7f7b7faf5fd468c51d17d7460f113640f31
Sxxjava <Sxxjava@126.com>
2016-06-14 18:13:06 +0800

    修改主界面

--- app/src/main/java/org/itsk/easedemo/MainActivity.java
+++ app/src/main/java/org/itsk/easedemo/MainActivity.java
@@ -2,12 +2,22 @@ package org.itsk.easedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.hyphenate.easeui.ui.EaseConversationListFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        container= (FrameLayout) findViewById(R.id.container);
+
+        EaseConversationListFragment fragment=new EaseConversationListFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container,fragment,"ConversationList").commit();
+
     }
 }
